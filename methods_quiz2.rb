8ceae586920d4c4ecb7b521ee5a87de8abbd8837
Alec Lamson <lamsonak@s.dcsdk12.org>
2015-09-11 09:53:51 -0600

    with out doubles pass

--- methods_quiz2.rb
+++ methods_quiz2.rb
@@ -1,5 +1,13 @@
-module MethodsQuiz
-	
+module MethodsQuiz2
+	def without_doubles (num1, num2, no_doubles)
+		sum = num1 + num2
+			return sum
+		if num1 == num2
+			no_doubles = true
+		else
+			false
+		end
+	end
 	
 
 end
--- methods_quiz2_test.rb
+++ methods_quiz2_test.rb
@@ -11,6 +11,16 @@ class MethodsQuizTest < MiniTest::Test
    	end.new
 	end
 
-	# TODO - write tests here
+	def test_without_doubles
+		assert_equal 3, @m.without_doubles(1, 2, false)
+		assert_equal 5, @m.without_doubles(3, 2, false)
+		assert_equal 4, @m.without_doubles(3, 1, false)
+		assert_equal 6, @m.without_doubles(2, 4, false)
+
+		assert_equal 4, @m.without_doubles(2 ,2, true)
+		assert_equal 6, @m.without_doubles(3 ,3, true)
+		assert_equal 12, @m.without_doubles(6 ,6, true)
+		
+	end
 
 end
