cbcba68d480f3503de81acb8f74db5624b84e63d
Alec Lamson <lamsonak@s.dcsdk12.org>
2015-09-14 10:22:20 -0600

    class ended

--- methods_quiz2.rb
+++ methods_quiz2.rb
@@ -10,22 +10,33 @@ module MethodsQuiz2
 	end
 	
 	def max_maybe (num1, num2)
-		if num1 == num2
-			return 0 
-		elsif num1 > num2
-			return num1
-		else
-			return num2
-		end
+		equal(num1, num2) ? 0 : (remainder?(num1, num2) ? (num1_more?(num1, num2) ? num2 : num1) : (num1_more(num1, num2) ? num_1 : num_2))
 	end
 	
-	#def squirrels_play? (temp, summer)
-	#	if temp >= 60 && temp <= 90
-	#		true
-		#elsif temp >= 60 && <= 100 && summer == true
-		#	true
-	#	else
-	#		false
-	#	end
-	#end
+	# def squirrels_play? (temp, summer)
+		# if temp >= 60 && temp <= 90
+			# true
+		# elsif temp >= 60 && <= 100 && summer == true
+			# true
+	  # else
+		# false
+		# end
+	# end
+
+	private
+		def no_doubles? x
+			x ? true : false
+		end
+		def roll? a,b,x
+			b == 6 && a == 6 % 5 ? true : false
+		end
+		def equal a,b
+			a == b ? true : false
+		end
+		def num1_more?
+			a > b ? true : false
+		end
+		def remainder? a,b
+			a % 5 == b % 5 ? true : false
+		end
 end
